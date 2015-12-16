2df8342ed681d98ece49c35306232963af36fdef
Priyansh <priyansh1990@gmail.com>
2015-12-16 22:52:40 +0530

    Play Ground Added

---
+++ SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+import UIKit
+
+//Array
+var todoStings:[String]=["Return Calls","Write Blos Post","Cook Dinner"] //You only can ass Strings
+var todo=["Return Calls","Write Blos Post","Cook Dinner",100] // You can add any data type
+
+
+// Insert
+todoStings+=["Pick up Laundry","Buy Shampoo"] // Multiple Objects Add
+todoStings.append("Buy Shoes")// Single object Add
+todoStings.insert("Call Mom", atIndex: 0)
+todoStings[2]="Do Dishes"// Will add at index 2.
+
+
+// Remove
+todoStings.removeAtIndex(5)
+todoStings.removeLast()
+todoStings.removeFirst()
+
+
+// Dictionary
+var countries=[ "CA":"Canada",
+                "BE":"Belgium",
+                "FR":"France"]
+countries["CA"]
+countries["IN"]
