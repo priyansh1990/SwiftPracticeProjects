42f8b0bf29ef9d933fe5b8485b8b19bb2522f4e8
Priyansh <priyansh1990@gmail.com>
2015-12-16 21:39:18 +0530

    Swift First Commit

---
+++ Swift Command Line/Swift Command Line/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Swift Command Line
+//
+//  Created by Priyansh Agarwal on 13/12/15.
+//  Copyright © 2015 Priyansh Agarwal. All rights reserved.
+//
+
+import Foundation
+
+var donutsLeft = 6
+while(donutsLeft > 0)
+{
+    print("You have \(donutsLeft) donuts left")
+    donutsLeft = donutsLeft - 1
+    print("You eat one donut")
+}
+print("You are all out of donuts :(")
+
