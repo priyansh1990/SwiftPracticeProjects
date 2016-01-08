4fc9b18308e9340135729ce187d7e77f245191a6
Priyansh <priyansh1990@gmail.com>
2016-01-08 22:19:40 +0530

    Code Removed

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -29,13 +29,6 @@ class ChecklistViewController: UITableViewController {
 
     
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
-    {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of rows
@@ -43,22 +36,6 @@ class ChecklistViewController: UITableViewController {
     }
 
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-    {
-        if let cell = tableView.cellForRowAtIndexPath(indexPath)
-        {
-            if cell.accessoryType == .None
-            {
-                cell.accessoryType = .Checkmark
-            }
-             else
-            {
-                cell.accessoryType = .None
-            }
-        }
-        
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
@@ -86,39 +63,21 @@ class ChecklistViewController: UITableViewController {
         return cell
     }
     
-    
-    
-
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
-    {
-        if editingStyle == .Delete
+        if let cell = tableView.cellForRowAtIndexPath(indexPath)
         {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            if cell.accessoryType == .None
+            {
+                cell.accessoryType = .Checkmark
+            }
+            else
+            {
+                cell.accessoryType = .None
+            }
         }
-        else if editingStyle == .Insert
-        {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
     // MARK: - Navigation
